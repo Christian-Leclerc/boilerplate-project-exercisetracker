115ea41cbd394edda18d89fc7795bed5ff6eb3e9
Christian-Leclerc <christianleclerc.info@gmail.com>
2023-04-12 14:48:07 -0400

    test: initial save

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', () => {
+  console.log("MongoDB database connection establishes successfully");
+});
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+console.log(process.env['MONGO_URI']);
+console.log(process.env.PORT);
 
 
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
